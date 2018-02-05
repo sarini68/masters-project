b865dce266a2f35a078be94c34f4dc1c686f89bf
MadViper <mkenc12@freeuni.edu.ge>
2018-02-05 09:51:17 +0200

    Add works with structure to DAL (Ref: #1)

--- data_access_layer.py
+++ data_access_layer.py
@@ -1,3 +1,5 @@
+import collections
+
 import neo4j.v1
 
 
@@ -6,6 +8,7 @@ class DAL(object):
     def __init__(self, driver: neo4j.v1.Driver):
         self._cases = []
         self._performers = []
+        self._works_with = []
 
         self.driver = driver
 
@@ -13,39 +16,9 @@ class DAL(object):
     def session(self) -> neo4j.v1.Session:
         return self.driver.session()
 
-    def fetch_performers(self):
-        PERFORMER_NAMES_QUERY = '''
-        match (c:case)-[:includes]->(act:activity)-[:performed_by]->(p:performer)
-        return distinct p.name as name order by p.name
-        '''
-
-        performer_names = self.run_query(PERFORMER_NAMES_QUERY)
-        self._performers = [record['name'] for record in performer_names]
-
-        return self._performers
-
-    @property
-    def performers(self):
-        return self._performers or self.fetch_performers()
-
-    def fetch_cases(self):
-        CASE_NAMES_QUERY = '''
-        match (c:case)-[:includes]->(act:activity)-[:performed_by]->(p:performer)
-        return distinct c.id as id order by c.id
-        '''
-
-        case_names = self.run_query(CASE_NAMES_QUERY)
-        self._cases = [record['id'] for record in case_names]
-
-        return self._cases
-
-    @property
-    def cases(self):
-        return self._cases or self.fetch_cases()
-
-    def run_query(self, query):
+    def run_query(self, query, *args, **kwargs):
         with self.session as session:
-            return session.run(query)
+            return session.run(query, *args, **kwargs)
 
     def seed_database(self):
         with self.session as s:
@@ -62,3 +35,60 @@ class DAL(object):
             '''
 
             s.run(graph_from_csv_query)
+
+    PERFORMER_NAMES_QUERY = '''
+    match (c:case)-[:includes]->(act:activity)-[:performed_by]->(p:performer)
+    return distinct p.name as name order by p.name
+    '''
+
+    def fetch_performers(self):
+        raw_performer_names = self.run_query(self.PERFORMER_NAMES_QUERY)
+        return [record['name'] for record in raw_performer_names]
+
+    @property
+    def performers(self):
+        if not self._performers:
+            self._performers = self.fetch_performers()
+        return self._performers
+
+    CASE_NAMES_QUERY = '''
+    match (c:case)-[:includes]->(act:activity)-[:performed_by]->(p:performer)
+    return distinct c.id as id order by c.id
+    '''
+
+    def fetch_cases(self):
+        raw_case_names = self.run_query(self.CASE_NAMES_QUERY)
+        return [record['id'] for record in raw_case_names]
+
+    @property
+    def cases(self):
+        if not self._cases:
+            self._cases = self.fetch_cases()
+        return self._cases
+
+    WORKS_WITH_QUERY = '''
+    match (c:case)-[:includes]->(act:activity)-[:performed_by]->(p:performer)
+    where c.id = {case}
+    return p.name as name, p.created as time, id(p) as id order by toFloat(time)
+    '''
+
+    WorksWithEntry = collections.namedtuple('WorksWithEntry',
+                                            ['performer', 'time', 'id'])
+
+    def fetch_works_with(self):
+        works_with = []
+        for case in self.cases:
+            performer, _id, time = [], [], []
+            query_result = self.run_query(self.WORKS_WITH_QUERY, {'case': case})
+            for record in query_result:
+                performer.append(record["name"])
+                time.append(record["time"])
+                _id.append(record["id"])
+            works_with.append(self.WorksWithEntry(performer, time, _id))
+        return works_with
+
+    @property
+    def works_with(self):
+        if not self._works_with:
+            self._works_with = self.fetch_works_with()
+        return self._works_with
--- main.py
+++ main.py
@@ -1,10 +1,16 @@
-from neo4j.v1 import GraphDatabase
+import json
+
+from neo4j.v1 import GraphDatabase, basic_auth
 
 from data_access_layer import DAL
 
-driver = GraphDatabase.driver("bolt://localhost:7687")
+driver = GraphDatabase.driver("bolt://localhost:7687",
+                              auth=basic_auth("neo4j", "123456"))
 
 dal = DAL(driver)
 dal.seed_database()
 print('Performers: {}'.format(dal.performers))
-print("Cases: {}".format(dal.cases))
+print('Cases: {}'.format(dal.cases))
+print(json.dumps(dal.works_with[0].id))
+print(json.dumps(dal.works_with[0].time))
+print(json.dumps(dal.works_with[0].performer))
