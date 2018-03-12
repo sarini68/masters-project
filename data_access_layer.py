527c4f56c4c650abb459ab8710e4be3a56745a1b
MadViper <mkenc12@freeuni.edu.ge>
2018-03-12 22:11:18 +0200

    Add build_relations function to DAL

--- data_access_layer.py
+++ data_access_layer.py
@@ -38,6 +38,44 @@ class DAL(object):
 
             s.run(graph_from_csv_query)
 
+        self.build_relations()
+
+    def build_relations(self):
+        # Build the works_with relation in the neo4j database.
+        # To avoid the insertion of wrong relations (e.g. rework, when John performed A at two different times)
+        # a check is added on the time of creation of performers, the first performer must be created before the second.
+        # To prevent from alphanumerical ordering, time of creation is casted to float.
+
+        query = '''
+        match
+            (c1)-[]->(a1)-[]->(p1:performer),
+            (c2)-[]->(a2)-[]->(p2:performer)
+        where
+            c1.id = {case} and
+            c2.id = {case} and
+            p1.name={namename1} and
+            p2.name={namename2} and
+            a1.name={actname1} and
+            a2.name={actname2} and
+            toFloat(p1.created) < toFloat(p2.created)
+        merge
+            (p1)-[w:works_with]->(p2)
+        return w
+        '''
+
+        for j, entry in enumerate(self.works_with):
+            for i in range(len(entry.performer) - 1):
+                self.run_query(
+                    query,
+                    {
+                        'case': entry.case,
+                        'namename1': entry.performer[i],
+                        'namename2': entry.performer[i + 1],
+                        'actname1': self.activity[j][i],
+                        'actname2': self.activity[j][i + 1]
+                    }
+                )
+
     PERFORMER_NAMES_QUERY = '''
     match (c:case)-[:includes]->(act:activity)-[:performed_by]->(p:performer)
     return distinct p.name as name order by p.name
